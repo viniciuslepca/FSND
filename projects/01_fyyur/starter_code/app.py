7161bc003a2bd1682814f12422affbfb8cc514a3
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-07-26 18:46:27 -0300

    Implemented missing fields in the Models, used migrations to update the database

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -25,41 +25,81 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db) # Used for migrations
 
-# TODO: connect to a local postgresql database -- DONE (through config.py)
+# DONE: connect to a local postgresql database -- done through config.py
+
+#----------------------------------------------------------------------------#
+# Relationship tables
+#----------------------------------------------------------------------------#
+# Relationship table between venues and genres
+venues_genres = db.Table('venues_genres',
+db.Column('venue_id', db.Integer, db.ForeignKey('venues.id'), primary_key=True),
+db.Column('genre_id', db.Integer, db.ForeignKey('genres.id'), primary_key=True)
+)
+
+# Relationship table between artists and genres
+artists_genres = db.Table('artists_genres',
+db.Column('artist_id', db.Integer, db.ForeignKey('artists.id'), primary_key=True),
+db.Column('genre_id', db.Integer, db.ForeignKey('genres.id'), primary_key=True)
+)
 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
 
 class Venue(db.Model):
-    __tablename__ = 'venues'
+  __tablename__ = 'venues'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String)
+  city = db.Column(db.String(120))
+  state = db.Column(db.String(120))
+  address = db.Column(db.String(120))
+  phone = db.Column(db.String(120))
+  image_link = db.Column(db.String(500))
+  facebook_link = db.Column(db.String(120))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+  # DONE: implement any missing fields, as a database migration using Flask-Migrate
+  website = db.Column(db.String(120))
+  seeking_talent = db.Column(db.Boolean)
+  seeking_description = db.Column(db.String(500))
+  # Relationships
+  shows = db.relationship('Show', backref='venue', lazy=True)
+  genres = db.relationship('Genre', secondary=venues_genres, backref=db.backref('venues', lazy=True))
 
 class Artist(db.Model):
-    __tablename__ = 'artists'
+  __tablename__ = 'artists'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String)
+  city = db.Column(db.String(120))
+  state = db.Column(db.String(120))
+  phone = db.Column(db.String(120))
+  genres = db.Column(db.String(120))
+  image_link = db.Column(db.String(500))
+  facebook_link = db.Column(db.String(120))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+  # DONE: implement any missing fields, as a database migration using Flask-Migrate
+  website = db.Column(db.String(120))
+  seeking_venue = db.Column(db.Boolean)
+  seeking_description = db.Column(db.String(500))
+  # Relationships
+  genres = db.relationship('Genre', secondary=artists_genres, backref=db.backref('artists', lazy=True))
+  shows = db.relationship('Show', backref='artist', lazy=True)
 
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+# DONE: Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+# Values for this table are added through the migration script that introduces it
+class Genre(db.Model):
+  __tablename__ = 'genres'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String())
+
+class Show(db.Model):
+  __tablename__ = 'shows'
+  id = db.Column(db.Integer, primary_key=True)
+  start_time = db.Column(db.DateTime)
+  artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
+  venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -513,6 +553,35 @@ if not app.debug:
 #----------------------------------------------------------------------------#
 # Launch.
 #----------------------------------------------------------------------------#
+
+# Populate the genres table
+with app.app_context():
+  if db.session.query(Genre).count() == 0:
+    genres = [
+      {'id': 1, 'name': 'Alternative'},
+      {'id': 2, 'name': 'Blues'},
+      {'id': 3, 'name': 'Classical'},
+      {'id': 4, 'name': 'Country'},
+      {'id': 5, 'name': 'Electronic'},
+      {'id': 6, 'name': 'Folk'},
+      {'id': 7, 'name': 'Funk'},
+      {'id': 8, 'name': 'Hip-Hop'},
+      {'id': 9, 'name': 'Heavy Metal'},
+      {'id': 10, 'name': 'Instrumental'},
+      {'id': 11, 'name': 'Jazz'},
+      {'id': 12, 'name': 'Musical Theatre'},
+      {'id': 13, 'name': 'Pop'},
+      {'id': 14, 'name': 'Punk'},
+      {'id': 15, 'name': 'R&B'},
+      {'id': 16, 'name': 'Reggae'},
+      {'id': 17, 'name': 'Rock n Roll'},
+      {'id': 18, 'name': 'Soul'},
+      {'id': 19, 'name': 'Other'}
+    ]
+    for genre in genres:
+      g = Genre(**genre)
+      db.session.add(g)
+    db.session.commit()
 
 # Default port:
 if __name__ == '__main__':
