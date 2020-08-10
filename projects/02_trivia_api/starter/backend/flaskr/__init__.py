803e0e2ecd4421b58c2de1725cc37decc191e536
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-08-10 20:59:42 -0300

    Implemented @app.after_request

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -19,8 +19,13 @@ def create_app(test_config=None):
   cors = CORS(app, resources={r'/*': {'origins': '*'}})
 
   '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
+  @DONE: Use the after_request decorator to set Access-Control-Allow
   '''
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization, true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+    return response
 
 
   '''
