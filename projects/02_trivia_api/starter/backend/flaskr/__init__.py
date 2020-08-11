6a2f61acd0f095ddbb1e012aaafa121f16b95ca7
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-08-10 21:19:45 -0300

    Wrote GET /categories

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -33,6 +33,15 @@ def create_app(test_config=None):
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
+  @app.route('/categories')
+  def get_categories():
+    categories = Category.query.order_by(Category.id).all()
+    formatted_categories = [category.format() for category in categories]
+
+    return jsonify({
+      'success': True,
+      'categories': formatted_categories
+    })
 
 
   '''
--- projects/02_trivia_api/starter/frontend/src/components/Question.js
+++ projects/02_trivia_api/starter/frontend/src/components/Question.js
@@ -19,9 +19,9 @@ class Question extends Component {
       <div className="Question-holder">
         <div className="Question">{question}</div>
         <div className="Question-status">
-          <img className="category" src={`${category}.svg`}/>
+          <img className="category" alt="Category" src={`${category}.svg`}/>
           <div className="difficulty">Difficulty: {difficulty}</div>
-          <img src="delete.png" className="delete" onClick={() => this.props.questionAction('DELETE')}/>
+          <img src="delete.png" className="delete" alt="Delete" onClick={() => this.props.questionAction('DELETE')}/>
           
         </div>
         <div className="show-answer button"
