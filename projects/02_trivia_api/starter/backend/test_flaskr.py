599d091b8fc42cd4a18e2ad36c62a1921dbb1909
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-08-11 01:05:58 -0300

    Wrote test for GET /categories, wrote bash script to run tests

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -15,8 +15,40 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://postgres:postgres@{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
+
+        # Setup default categories
+        self.categories = [
+            {
+                'id': 1,
+                'type': 'Science'
+            },
+            {
+                'id': 2,
+                'type': 'Art'
+            },
+            {
+                'id': 3,
+                'type': 'Geography'
+            },
+            {
+                'id': 4,
+                'type': 'History'
+            },
+            {
+                'id': 5,
+                'type': 'Entertainment'
+            },
+            {
+                'id': 6,
+                'type': 'Sports'
+            },
+            {
+                'id': 1,
+                'type': 'Science'
+            },
+        ]
 
         # binds the app to the current context
         with self.app.app_context():
@@ -33,6 +65,15 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
+        for index, category in enumerate(data['categories']):
+            self.assertEqual(category['id'], self.categories[index]['id'])
+            self.assertEqual(category['type'], self.categories[index]['type'])
 
 
 # Make the tests conveniently executable
