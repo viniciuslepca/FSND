fff80e074ad9fa65304cb52c25c236763cbc0cd1
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-08-11 02:53:47 -0300

    Made input fields required in the frontend

--- projects/02_trivia_api/starter/frontend/src/components/FormView.js
+++ projects/02_trivia_api/starter/frontend/src/components/FormView.js
@@ -70,11 +70,11 @@ class FormView extends Component {
         <form className="form-view" id="add-question-form" onSubmit={this.submitQuestion}>
           <label>
             Question
-            <input type="text" name="question" onChange={this.handleChange}/>
+            <input type="text" name="question" onChange={this.handleChange} required/>
           </label>
           <label>
             Answer
-            <input type="text" name="answer" onChange={this.handleChange}/>
+            <input type="text" name="answer" onChange={this.handleChange} required/>
           </label>
           <label>
             Difficulty
